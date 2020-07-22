f0a877de6f27f7ce80d3cbc97aa506aca00f5e40
cola119 <k.cola119@ezweb.ne.jp>
2020-07-22 23:59:34 +0900

    add jest environment

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json",
+    },
+  },
+  testMatch: ["**/test/**/*.test.ts"],
+};
---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+describe("sample", () => {
+  it("test", () => {
+    expect(1).toBe(1);
+  });
+});
